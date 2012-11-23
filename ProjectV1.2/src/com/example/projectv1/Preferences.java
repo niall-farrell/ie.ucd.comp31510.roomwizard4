7aed7aa3069650b1d93a9d1b305b02c6c5779541
emersonderek <emersonderek@yahoo.com>
2012-11-23 10:02:53 +0000

    Preferences Screen
    Just the screen

--- ProjectV1.2/src/com/example/projectv1/Preferences.java
+++ ProjectV1.2/src/com/example/projectv1/Preferences.java
@@ -1,11 +1,61 @@
 package com.example.projectv1;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+public class Preferences extends PreferenceActivity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {       
+        super.onCreate(savedInstanceState);       
+        addPreferencesFromResource(R.xml.preferences);       
+
+
+ //       Preference button = (Preference)findPreference("backButtons");
+        Preference button = (Preference)getPreferenceManager().findPreference("backButtons");      
+        
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(Preference arg0) { 
+                            //Intent intent = new Intent();
+                            //intent.setClass(this,Menu.class);
+                            //startActivity(intent);
+                        	Log.v("back","button");
+                			Intent openMenu = new Intent("com.project.MENU");
+                			startActivity(openMenu);
+
+                            finish();
+         	                            return true;
+                        }
+                    });
+    
+    }
+     
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 0, 0, "Show current settings");
+        return super.onCreateOptionsMenu(menu);
+    }
+ 
+    @Override
+public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+//                startActivity(new Intent(this, ShowSettingsActivity.class));
+                return true;
+        }
+        return false;
+    }
+     
+}
+/*
 public class Preferences extends Activity {
 
 	@Override
@@ -42,3 +92,4 @@ public class Preferences extends Activity {
 		}
 	}
 }
+*/
