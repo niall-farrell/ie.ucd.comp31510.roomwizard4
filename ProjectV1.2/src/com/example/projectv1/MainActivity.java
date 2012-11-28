600ed015b82c588c36e25963756dd62d11fb7bd7
nailmustafin <nail.mustafin@ucdconnect.ie>
2012-11-28 20:24:27 +0000

    CJ: updated MainActivity with methods to start next day and send states, plus stat updates

--- ProjectV1.2/src/com/example/projectv1/MainActivity.java
+++ ProjectV1.2/src/com/example/projectv1/MainActivity.java
@@ -3,52 +3,46 @@ package com.example.projectv1;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Iterator;
-import java.util.Locale;
+import java.util.List;
 import java.util.Map;
-
-import com.example.projectv1.timeline.TimelineImageView;
-import com.example.projectv1.ClassBooking;
-
-import android.net.Uri;
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.Property;
-import net.fortuna.ical4j.model.TimeZoneRegistry;
-import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
-import net.fortuna.ical4j.util.Calendars;
-import net.fortuna.ical4j.util.CompatibilityHints;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.projectv1.timeline.TimelineImageView;
 
 public class MainActivity extends Activity{
 	// Create our linkedlist of class bookings
 	ArrayList<ClassBooking> cb = new ArrayList<ClassBooking>();
+	
+	private SharedPreferences pref;
 	
 	Boolean occupied = true;
 	Calendar c = Calendar.getInstance();
@@ -58,6 +52,14 @@ public class MainActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// initialise pref
+		pref = getPreferences(MODE_PRIVATE);
+		
+		// update restart counter (currently based on creation of main activity)
+        SharedPreferences.Editor editRestart = pref.edit();
+        editRestart.putInt("nRestart", pref.getInt("nRestart", 0) + 1);
+
 		
 		try {
 			cb = getClassBooking();
@@ -131,6 +133,10 @@ public class MainActivity extends Activity{
 					
 					Toast.makeText(getApplicationContext(),
 							"You clicked at: " + positionX + "\n" + message, Toast.LENGTH_SHORT).show();
+					
+					// update interaction statistic
+					SharedPreferences.Editor editInteract = pref.edit();
+			        editInteract.putInt("nInteract", pref.getInt("nInteract", 0) + 1);
 					
 					return true;
 				}
@@ -256,5 +262,59 @@ public class MainActivity extends Activity{
 
 			
 			return cb;
+		}
+		
+		// creates AlarmManager object to restore device from standby at specified time
+		public void createStartAlarm(int hour, int minute)
+		{
+			// create calendar object for tomorrow using time parameters
+	        Calendar cal = Calendar.getInstance();
+	        cal.setTimeInMillis(System.currentTimeMillis());
+	        cal.add(Calendar.DATE, 1);
+	        cal.set(Calendar.HOUR_OF_DAY, hour);
+	        cal.set(Calendar.MINUTE, minute);
+	        cal.set(Calendar.SECOND, 0);
+	        cal.set(Calendar.MILLISECOND, 0);
+			
+			// initialise intent to start boot app service
+	        Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+
+	        PendingIntent pendingIntent
+	         = PendingIntent.getBroadcast(getBaseContext(),
+	           0, myIntent, 0);
+	      
+	        // bind intent to alarm
+	        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+	        
+	        alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+		}
+		
+		// method to send statistics report to server
+		// this is called at the end of the day prior to standby
+		public void sendStats()
+		{
+	        // Create HttpClient and header
+	        HttpClient httpclient = new DefaultHttpClient();
+	        HttpPost httppost = new HttpPost("http://localhost/app/processStats.php"); // ## update to live server
+
+	        try {
+	            // Add your data
+	            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
+	            nameValuePairs.add(new BasicNameValuePair("deviceid", "1"));
+	            nameValuePairs.add(new BasicNameValuePair("restart", "" + pref.getInt("restart", 0)));
+	            nameValuePairs.add(new BasicNameValuePair("interact", "" + pref.getInt("interact", 0)));
+	            nameValuePairs.add(new BasicNameValuePair("network", "" + pref.getInt("network", 0)));
+	            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+	            // Execute HTTP Post Request
+	            HttpResponse response = httpclient.execute(httppost);
+	            
+	        } catch (Exception e)
+	        {
+	            // update nNetwork in pref
+	            SharedPreferences pref = getPreferences(MODE_PRIVATE);
+	            SharedPreferences.Editor editor = pref.edit();
+	            editor.putInt("nInteract", pref.getInt("nInteract", 0) + 1);
+	        }
 		}
 }
